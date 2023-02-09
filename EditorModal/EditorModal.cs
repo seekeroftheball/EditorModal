6a1d7fbad9f62545c1271faee47dcc226d75c572
Jordan Wilgus <jordan.wilgus@gmail.com>
2023-02-08 19:15:21 -0500

    Add files via upload

---
+++ EditorModal/EditorModal.cs
@@ -0,0 +1,36 @@
+//Author: https://github.com/seekeroftheball
+
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorModal
+{
+    public class EditorPopupModal : EditorWindow
+    {
+        private struct WindowBounds
+        {
+            public const float WindowWidth = 242;
+            public const float WindowHeight = 72;
+
+            public static Vector2 WindowSize = new(WindowWidth, WindowHeight);
+        }
+
+        private EditorPopupModal()
+        {
+            minSize = WindowBounds.WindowSize;
+            maxSize = WindowBounds.WindowSize;
+        }
+
+        private void OnGUI() => DrawWindow();
+
+        private void OnInspectorUpdate() => Repaint();
+
+        private void DrawWindow()
+        {
+            GUILayout.Label("Here's some text in a pop-up window.\n\nPress Close to dismiss this message.", EditorStyles.wordWrappedLabel);
+
+            if (GUILayout.Button("Close"))
+                Close();
+        }
+    }
+}
